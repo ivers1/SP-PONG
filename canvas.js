60c171ffa56dcaf1a4f243feb19242b732fe1532
ivers1 <ivers1984@hotmail.com>
2020-09-17 15:11:06 +0200

    animation start

--- canvas.js
+++ canvas.js
@@ -6,7 +6,33 @@ ctx.fillRect(5, 100, 25, 100);
 ctx.fillStyle = 'blue';
 ctx.fillRect(570, 100, 25, 100);
 
-ctx.beginPath();
-ctx.arc(canvas.width / 2, canvas.height / 2, 10, 0, Math.PI * 2);
-ctx.fillStyle = 'black';
-ctx.fill();
+//ctx.beginPath();
+//ctx.arc(canvas.width / 2, canvas.height / 2, 10, 0, Math.PI * 2);
+//ctx.fillStyle = 'black';
+//ctx.fill();
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 20,
+    dx: 5,
+    dy: 4
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+}
+
+function update() {
+clearRect    
+drawBall();
+
+ball.x += ball.dx;    
+    
+requestAnimationFrame(update); 
+}
+
+update();
